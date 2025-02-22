b77c183b64c38ef0b86ef4168a4e7c77f17b899c
Klevis Imeri <klevisimeri11@gmail.com>
2025-02-22 11:21:24 +0100

    Put the main function into the test class

--- Debug.java
+++ Debug.java
@@ -89,19 +89,5 @@ public class Debug {
         }
         System.err.println("]");
     }
-
-    public static void main(String[] args) {
-        class Example {
-            int id = 1;
-            String name = "Test";
-            double score = 99.5;
-        }
-
-        List<Integer> list = Arrays.asList(1, 2, 3);
-        Map.Entry<String, Integer> pair = Map.entry("hello", 42);
-        Example obj = new Example();
-
-        debug("Test", 123, 45.67, true, list, pair, obj);
-    }
 }
 
